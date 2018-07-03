c574efa304f43abcc81ea0dc54be3ac931f21fa3
monicadhanasekaran <nddmonica2311@gmail.com>
2018-07-03 22:40:11 +0530

    largest

---
+++ guvi4.py
@@ -0,0 +1,14 @@
+﻿u=raw_input()
+v=raw_input()
+w=raw_input()
+a=int(u)
+b=int(v)
+c=int(w)
+if((a>=b) and (a>=c)):
+   print a
+elif(b>=a) and (b>=c):
+	print b
+elif(c>=a) and (c>=b):	
+    print c
+else:
+   print("no it is not a alphabet")
