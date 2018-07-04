cb2a1c2ecd65182c9af3cfe9aa3f79d677fd6247
monica <40767164+monicadhanasekaran@users.noreply.github.com>
2018-07-05 01:04:11 +0530

    Update guvi 7.py

--- guvi 7.py
+++ guvi 7.py
@@ -1,19 +1,10 @@
 N=raw_input()
-
 if N.isalpha():
-	
  print("invalid")
-
 else:
-	
   N=int(N)
-	
   count=0
-	
     while N!=0:
-   
-    N=int(N/10)
-	
-    count=count+1
-   
-  print(count)
+      N=int(N/10)
+      count=count+1
+   print(count)
