9d827633f15c8685ca0fadb040c94debf2e5595f
monicadhanasekaran <nddmonica2311@gmail.com>
2018-07-07 13:45:23 +0530

    power

---
+++ guvi1013.py
@@ -0,0 +1,9 @@
+a=int(raw_input())
+re=0
+while (n>=0):
+ re=rev*10
+ re=re\10
+if(a==re)  
+ print("palindrome")
+else:
+ print("palindrome")
---
+++ guvi14.py.txt
@@ -0,0 +1,6 @@
+a=raw_input()
+b=raw_input()
+c=int(a)
+d=int(b)
+v=c**d
+print v
