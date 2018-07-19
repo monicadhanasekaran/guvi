b20b4518b3425e1d45f8de062f6e89e53a921e3d
monica <40767164+monicadhanasekaran@users.noreply.github.com>
2018-07-19 10:44:32 +0530

    Update guviholiday.py

--- guviholiday.py
+++ guviholiday.py
@@ -1,13 +1,7 @@
 a=raw_input()
-
 if(a=="sunday" or a=="saturday"):
-
 	print("yes")
-
 elif(a=="monday" or a=="tuesday" or a=="wednesday" or a=="thursday" or a=="friday"):
-
 	print("no")
-
 else:
-
 	print("invalid")
